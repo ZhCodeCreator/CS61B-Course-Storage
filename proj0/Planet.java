80baf7c422ea7f3592f2446d32ab0f2ad05f4aa0
Ling <2668636119@qq.com>
2025-07-10 17:28:57 +0800

    proj0-second

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -4,9 +4,9 @@ public class Planet {
     public double xxVel;
     public double yyVel;
     public double mass;
-    private String imgFileName;
+    public String imgFileName;
 
-    public static final double G = 6.67e-11;
+    private static final double G = 6.67e-11;
 
     public Planet(double xP, double yP, double xV,
                   double yV, double m, String img) {
